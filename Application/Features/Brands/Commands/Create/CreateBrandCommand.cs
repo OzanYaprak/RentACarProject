022bcc700d0d727b3e717ae4547f6b84ed7a5ac8
Ozan Yaprak <oznyprk@gmail.com>
2026-04-23 23:07:42 +0300

    Implement ICacheRemoverRequest in CreateBrandCommand
    Added ICacheRemoverRequest to CreateBrandCommand, introducing CacheKey and BypassCache properties (currently not implemented). Also added the required caching namespace import.

--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,9 +1,14 @@
-﻿using Core.Application.Pipelines.Transaction;
+﻿using Core.Application.Pipelines.Caching;
+using Core.Application.Pipelines.Transaction;
 using MediatR;
 
 namespace Application.Features.Brands.Commands.Create;
 
-public class CreateBrandCommand : IRequest<CreatedBrandResponse>, ITransactionalRequest
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>, ITransactionalRequest , ICacheRemoverRequest
 {
     public string Name { get; set; } = null!;
+
+    public string CacheKey => throw new NotImplementedException();
+
+    public bool BypassCache => throw new NotImplementedException();
 }
